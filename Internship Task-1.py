7ef0135426b1763a8b279ddf4ea5754e8b084127
Shashwat-02 <107566546+Shashwat-02@users.noreply.github.com>
2024-03-12 19:42:29 +0530

    Add files via upload

---
+++ Internship Task-1.py
@@ -0,0 +1,43 @@
+# Creating a list
+my_list = [1, 2, 3, 4, 5]
+print("List:", my_list)
+
+# Adding an element to the list
+my_list.append(6)
+print("After adding element to list:", my_list)
+
+# Removing an element from the list
+my_list.remove(3)
+print("After removing element from list:", my_list)
+
+# Modifying an element in the list
+my_list[2] = 10
+print("After modifying element in list:", my_list)
+
+# Creating a dictionary
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+print("Dictionary:", my_dict)
+
+# Adding an element to the dictionary
+my_dict['d'] = 4
+print("After adding element to dictionary:", my_dict)
+
+# Removing an element from the dictionary
+del my_dict['b']
+print("After removing element from dictionary:", my_dict)
+
+# Modifying an element in the dictionary
+my_dict['a'] = 100
+print("After modifying element in dictionary:", my_dict)
+
+# Creating a set
+my_set = {1, 2, 3, 4, 5}
+print("Set:", my_set)
+
+# Adding an element to the set
+my_set.add(6)
+print("After adding element to set:", my_set)
+
+# Removing an element from the set
+my_set.remove(3)
+print("After removing element from set:", my_set)
